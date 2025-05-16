5abd640e953af49d6d4837f7761c70bb4d02b997
Lihao <1057622788@qq.com>
2025-05-15 23:25:42 -1200

    Initial commit with Survis and GitHub Pages setup

---
+++ survis/src/data/generated/bib.js
@@ -0,0 +1,118 @@
+﻿const generatedBibEntries = {
+    "Beck2016SurVis": {
+        "author": "Beck, F. and Koch, S. and Weiskopf, D.",
+        "doi": "10.1109/TVCG.2015.2467757",
+        "journal": "IEEE Transactions on Visualization and Computer Graphics",
+        "keywords": "type:Survis & Literacy",
+        "number": "1",
+        "title": "Visual analysis and dissemination of scientific literature collections with SurVis",
+        "type": "article",
+        "url": "https://doi.org/10.1109/TVCG.2015.2467757",
+        "volume": "22",
+        "year": "2016"
+    },
+    "Brooks2004Distributed": {
+        "author": "Brooks, C. and Donnell, M.",
+        "doi": "10.1111/j.1467-8659.2004.00754.x",
+        "journal": "Computer Graphics Forum",
+        "keywords": "type:Collaborative visualization",
+        "number": "4",
+        "pages": "223--232",
+        "title": "Distributed and Collaborative Visualization",
+        "type": "article",
+        "url": "https://doi.org/10.1111/j.1467-8659.2004.00754.x",
+        "volume": "23",
+        "year": "2004"
+    },
+    "Elsayed2020Survey": {
+        "author": "Elsayed, N. and Mohamed, M. and Raml, F.",
+        "doi": "10.14569/IJACSA.2020.0111221",
+        "journal": "International Journal of Advanced Computer Science and Applications",
+        "keywords": "type:Survis & Literacy",
+        "number": "12",
+        "title": "Visualization techniques for literature review: A survey",
+        "type": "article",
+        "url": "https://doi.org/10.14569/IJACSA.2020.0111221",
+        "volume": "11",
+        "year": "2020"
+    },
+    "Gaver2024CardToolResilience": {
+        "author": "Gaver, B. and Boucher, A. and Pennington, S.",
+        "booktitle": "Proceedings of the 2024 CHI Conference on Human Factors in Computing Systems",
+        "doi": "10.1145/3613904.3642686",
+        "keywords": "type:Card-based design HCI",
+        "title": "Designing a Card-Based Design Tool to Bridge Academic Research & Design Practice For Societal Resilience",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1145/3613904.3642686",
+        "year": "2024"
+    },
+    "Halskov2018CardTools": {
+        "author": "Halskov, K. and Dalsgaard, P.",
+        "booktitle": "Proceedings of DRS 2018 International Conference",
+        "doi": "10.21606/drs.2018.284",
+        "keywords": "type:Card-based design HCI",
+        "title": "Card-based Tools for Creative and Systematic Design",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.21606/drs.2018.284",
+        "year": "2018"
+    },
+    "Huang2023DesignCards": {
+        "author": "Huang, C. and Chen, Y.",
+        "booktitle": "Proceedings of the 2023 CHI Conference on Human Factors in Computing Systems",
+        "doi": "10.1145/3544548.3580712",
+        "keywords": "type:Collaborative visualization",
+        "title": "What is in the Cards: Exploring Uses, Patterns, and Trends in Design Cards",
+        "type": "inproceedings",
+        "url": "https://doi.org/10.1145/3544548.3580712",
+        "year": "2023"
+    },
+    "Isenberg2011CollabVis": {
+        "author": "Isenberg, P. and Elmqvist, N. and Scholtz, J. and Cernea, D. and Ma, K. and Hagen, H.",
+        "doi": "10.1177/1473871611412817",
+        "journal": "Information Visualization",
+        "keywords": "type:Collaborative visualization",
+        "number": "4",
+        "pages": "310--326",
+        "title": "Collaborative visualization: Definition, challenges, and research agenda",
+        "type": "article",
+        "url": "https://doi.org/10.1177/1473871611412817",
+        "volume": "10",
+        "year": "2011"
+    },
+    "Lucero2019CardTools": {
+        "author": "Lucero, A. and Dalsgaard, P. and Halskov, K. and Buur, J.",
+        "doi": "10.1016/j.destud.2019.04.002",
+        "journal": "Design Studies",
+        "keywords": "type:Card-based design HCI",
+        "pages": "125--154",
+        "title": "Card-based design tools: A review and analysis of 155 card decks for designers and designing",
+        "type": "article",
+        "url": "https://doi.org/10.1016/j.destud.2019.04.002",
+        "volume": "63",
+        "year": "2019"
+    },
+    "Vartiainen2024ElementaryDesign": {
+        "author": "Vartiainen, J. and Aksela, M.",
+        "doi": "10.1007/s10798-024-09916-3",
+        "journal": "International Journal of Technology and Design Education",
+        "keywords": "type:Card-based design HCI",
+        "title": "A card-based design tool for supporting design thinking in elementary students",
+        "type": "article",
+        "url": "https://doi.org/10.1007/s10798-024-09916-3",
+        "year": "2024"
+    },
+    "myers2000past": {
+        "author": "Myers, Brad A. and Hudson, Scott E. and Pausch, Randy",
+        "doi": "10.1145/344949.344959",
+        "journal": "ACM Transactions on Computer-Human Interaction",
+        "keywords": "type:Collaborative visualization",
+        "number": "1",
+        "pages": "3--28",
+        "publisher": "Association for Computing Machinery",
+        "title": "Past, present, and future of user interface software tools",
+        "type": "article",
+        "url": "https://doi.org/10.1145/344949.344959",
+        "volume": "7",
+        "year": "2000"
+    }
+};
